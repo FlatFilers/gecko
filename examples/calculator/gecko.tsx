981d09cbe2ed2d2867d628518b2c70fe3a45dd3b
Nate Ferrero <nate@tagme.in>
2024-08-06 14:03:40 -0400

    0.1.6

--- examples/calculator/gecko.tsx
+++ examples/calculator/gecko.tsx
@@ -68,7 +68,7 @@ export default function () {
                             file.pathSegments?.[
                               file.pathSegments.length - 1
                             ]
-                          }' (${file.path ? s.read(`components/${file.name}`).content?.length ?? '(empty)' : '(none)'}b)`}
+                          }' (${file.content.length ? '(empty)' : '(none)'}b)`}
                         </Text>
                       ))}
                   </File>
